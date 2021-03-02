64525bc8cf312d2630ebbed839d8e414d372f3e0
Allie Brummer <73483482+allisonbrummer@users.noreply.github.com>
2021-03-02 14:16:04 -0500

    Fix version

--- src/main/scala/io/github/allisonbrummer/Logger.scala
+++ src/main/scala/io/github/allisonbrummer/Logger.scala
@@ -4,7 +4,7 @@ package io.github.allisonbrummer
   * Provides classes to write logs to various systems
   * 
   * @author Allison Brummer
-  * @version 0.1.0
+  * @version 0.0.1
   */
 package object logger {
 
